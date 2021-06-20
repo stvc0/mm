ffeea8d5798517b37975b3344129b5d1d9518413
stvc0 <spcrtr@gmail.com>
2021-06-20 14:57:35 -0400

    Added color

--- 2120/objects.r
+++ 2120/objects.r
@@ -10,5 +10,5 @@ raw <- read_xlsx("fightData.xlsx")  # The raw data
 data <- raw
 
 graph <- ggplot(data) +
-geom_point(aes(Male, Animal)) +
-geom_point(aes(Female, Animal))
+geom_point(aes(Male, Animal, color = "blue")) +
+geom_point(aes(Female, Animal, color = "orange"))
