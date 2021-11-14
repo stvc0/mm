938d76609ba87681a61853d16d46b64c5545941a
erntan <tanshuern@gmail.com>
2021-11-14 10:34:46 -0700

    Initial commit.

---
+++ 2101/objects.r
@@ -0,0 +1,11 @@
+#
+
+# Trail Count Data: RTC is conducting weekly analyses of national trail usage based on data gathered from RTC's 31 trali counters distributed across the country.
+
+
+
+library("ggplot2")
+library("leaflet")
+library("rgdal")
+library("dplyr")
+library("viridis")
--- 2120/objects.r
+++ 2120/objects.r
@@ -14,3 +14,7 @@ graph <- ggplot(data) +
     geom_point(aes(Female, Animal, color = "orange")) +
     theme(axis.title.x = element_blank(), axis.title.y = element_blank()) +
     labs(title = "What animal could you beat in a fight?") +
+    scale_x_continuous(breaks = , labels = c("0%", "10", "20", "30", "40", "50", "60", "70", "80"), expand = c(0,0))
+
+
+plot(graph)
